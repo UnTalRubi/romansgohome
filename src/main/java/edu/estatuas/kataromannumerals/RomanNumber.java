eb57c8f4faffb1c81459793d825cdf6a52c8debb
Rubi Quintas Alonso <untalrubi@gmail.com>
2025-05-14 12:07:09 +0200

    feat(RomanNumber): Create RomanNumber class
    Created the class for the RomanNumber and Sum Test

---
+++ src/main/java/edu/estatuas/kataromannumerals/RomanNumber.java
@@ -0,0 +1,12 @@
+package edu.estatuas.kataromannumerals;
+
+public class RomanNumber {
+
+    String romanAnnotation;
+
+    Integer decimalAnnotation;
+
+    public RomanNumber(String romanAnnotation) {
+        this.romanAnnotation = romanAnnotation;
+    }
+}
---
+++ src/test/java/edu/estatuas/kataromannumerals/Test/NumeroRomanoTest.java
@@ -0,0 +1,44 @@
+package edu.estatuas.kataromannumerals.Test;
+
+import org.junit.jupiter.api.Test;
+
+import edu.estatuas.kataromannumerals.RomanNumber;
+
+import org.junit.jupiter.api.Tag;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class NumeroRomanoTest {
+
+    public RomanNumber numeroRomano;
+
+    /**
+     * Grupos sumatorios M, C, X, I
+     */
+
+    @ParameterizedTest
+    @CsvSource({
+            "1000,  M",
+            "2000,  UMMU",
+            "3000,  UMMMU"
+    })
+
+    @Test
+    @Tag("sumatorio")
+    public void grupo_M_test(Short decimal, String roman) {
+
+        // String testCase = "M";
+        numeroRomano = new RomanNumber(roman);
+        assertEquals(decimal, numeroRomano.toDecimal());
+
+        /**
+         * El caso MMMM es control de errores
+         * y no puede estar en el test de la logica
+         * Asumimos que la entrada es correcta.
+         * Sino, hay que programar la gestion de errores
+         */
+    }
+}
