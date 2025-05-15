c5db943c17d7e792dade01ac4f8a8b52225932cf
KanekiiCat <alex123viri@gmail.com>
2025-05-15 17:42:06 +0200

    test(RomanDecoder): Add new tests

--- src/test/java/edu/estatuas/kataromannumerals/Test/NumeroRomanoTest.java
+++ src/test/java/edu/estatuas/kataromannumerals/Test/NumeroRomanoTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class NumeroRomanoTest {
 
@@ -38,5 +39,115 @@ public class NumeroRomanoTest {
          * Asumimos que la entrada es correcta.
          * Sino, hay que programar la gestion de errores
          */
+
+         
     }
+    @Test
+    @Tag("sumatorio")
+    public void tres_repeticiones_C_test() {
+
+        String testCase = "UMMMUCCCU";
+        numeroRomano = new RomanNumber(testCase);
+        assertEquals("3300", numeroRomano.toDecimal().toString());
+    }
+
+    @Test
+    @Tag("sumatorio")
+    public void tres_repeticiones_X_test() {
+
+        String testCase = "UMMMUXXXU";
+        numeroRomano = new RomanNumber(testCase);
+
+        assertEquals("3030", numeroRomano.toDecimal().toString());
+    }
+    @Test
+    @Tag("sumatorio")
+    public void tres_repeticiones_I_test() {
+
+        String testCase = "UMMMUIIIU";
+        numeroRomano = new RomanNumber(testCase);
+
+        assertEquals("3003", numeroRomano.toDecimal().toString());
+    }
+    @Test
+    @Tag("sumatorio")
+    public void una_D_test() {
+
+        String testCase = "UMMMUDUIIIU";
+        numeroRomano = new RomanNumber(testCase);
+        assertEquals("3503", numeroRomano.toDecimal().toString());
+
+        testCase = "MMMUCDUIIIU";
+        numeroRomano = new RomanNumber(testCase);
+        assertNotEquals("3503", numeroRomano.toDecimal().toString());
+    }
+    /**
+     * Grupos sustractivos
+     * IV(4), IX(9), 
+     * XL(40), XC(90), 
+     * CD(400), CM(900)
+     */
+
+    @Test
+    @Tag("sustractivo")
+    public void grupo_C_DM_test() {
+
+        String testCase = "UCDU";
+        numeroRomano = new RomanNumber(testCase);
+        assertEquals("400", numeroRomano.toDecimal().toString());
+
+        testCase = "UCMU";
+        numeroRomano = new RomanNumber(testCase);
+        assertEquals("900", numeroRomano.toDecimal().toString());
+    }
+    @Test
+    @Tag("sustractivo")
+    public void grupo_X_LC_test() {
+
+        String testCase = "UXLU";
+        numeroRomano = new RomanNumber(testCase);
+        assertEquals("40", numeroRomano.toDecimal().toString());  
+
+        testCase = "UXCU";
+        numeroRomano = new RomanNumber(testCase);
+        assertEquals("90", numeroRomano.toDecimal().toString());        
+    }
+    @Test
+    @Tag("sustractivo")
+    public void grupo_I_VX_test() {
+
+        String testCase = "UIVU";
+        numeroRomano = new RomanNumber(testCase);
+        assertEquals("4", numeroRomano.toDecimal().toString());  
+
+        testCase = "UIXU";
+        numeroRomano = new RomanNumber(testCase);
+        assertEquals("9", numeroRomano.toDecimal().toString());  
+    }
+    @Test
+    @Tag("sustractivo")
+    public void grupos_sumatorios_tres_digitos_test() {
+        String testCase = "MMMDCCCLXXXVIII"; // 3888
+        numeroRomano = new RomanNumber(testCase);
+        assertEquals("3888", numeroRomano.toDecimal().toString());
+    }
+    @Test
+    @Tag("sustractivo")
+    public void grupos_sumatorios_test() {
+        String testCase = "MMDCCLXXVII"; // 2777
+        numeroRomano = new RomanNumber(testCase);
+        assertEquals("2777", numeroRomano.toDecimal().toString());
+    }
+    @Test
+    @Tag("sustractivo")
+    public void grupos_substractivos_test() {
+        String testCase = "CDXLIV"; // 444
+        numeroRomano = new RomanNumber(testCase);
+        assertEquals("444", numeroRomano.toDecimal().toString());
+
+        testCase = "CDXXXIX"; // 439
+        numeroRomano = new RomanNumber(testCase);
+        assertEquals("439", numeroRomano.toDecimal().toString());
+    }
+
 }
